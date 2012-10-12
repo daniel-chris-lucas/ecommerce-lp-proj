ae9ac01983453322ad4137df29de9945f24a16c3
Daniel Lucas <daniel.chris.lucas@gmail.com>
2012-10-12 22:57:12 +0200

    added conception docs to the project root

---
+++ fuel/app/logs/2012/10/11.php
@@ -0,0 +1,5 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+Warning - 2012-10-11 12:33:57 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-10-11 12:55:40 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-10-11 12:55:40 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
