410f6a0359582b7b44dbc34c9cccc2a910414582
Severine Stempin <mangagirl2@gmail.com>
2012-10-23 15:17:38 +0200

    modification du gitignore

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -7,8 +7,8 @@ return array(
 	'default' => array(
 		'connection'  => array(
 			'dsn'        => 'mysql:host=localhost;dbname=e_commerce',
-			'username'   => 'root',
-			'password'   => 'root',
+			'username'   => 'e_11_jse0900840',
+			'password'   => 'je_11_jse0900840j',
 		),
 	),
 );
