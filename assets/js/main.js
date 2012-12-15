c2cf63041dec24406a62648b4cf49487486ab221
Daniel Lucas <daniel.chris.lucas@gmail.com>
2012-12-15 08:39:47 +0100

    added zipped version of app to docs

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,4 +35,45 @@
 
 	$( '.fancybox' ).fancybox();
 
+
+	$(".slider_prev").click(function(){
+	var udaljeno = $("#slider ul").css("left");
+	if(udaljeno!='0px'){
+	$('#slider ul').animate({left: '+=1170'});
+	var palica = $(".handle").css("left");
+	if(palica == '195px'){
+	$(".handle").animate({left:'0'});
+	}
+	if(palica == '380px'){
+	$(".handle").animate({left:'195'});
+	}
+	if(palica == '570px'){
+	$(".handle").animate({left:'380'});
+	}
+	if(palica == '760px'){
+	$(".handle").animate({left:'570'});
+	}
+	}
+	});
+	//desna
+	$(".slider_next").click(function(){
+	var udaljeno = $("#slider ul").css("left");
+	if(udaljeno!='-5850px'){
+	$('#slider ul').animate({left: '-=1170'});
+	var palica = $(".handle").css("left");
+	if(palica == '0px'){
+	$(".handle").animate({left:'1170'});
+	}
+	if(palica == '1170x'){
+	$(".handle").animate({left:'2340'});
+	}
+	if(palica == '2340px'){
+	$(".handle").animate({left:'3510'});
+	}
+	if(palica == '3510px'){
+	$(".handle").animate({left:'4680'});
+	}
+	}
+	});
+
 })( jQuery );
--- fuel/app/views/layouts/slider.php
+++ fuel/app/views/layouts/slider.php
@@ -4,20 +4,56 @@
         <div class="span12" id="slider">
             <a href="#" class="slider_prev">Prev</a>
             <a href="#" class="slider_next">Next</a>
-            <div class="left_part">
-                <?php echo Asset::img( 'example-product.png', array( 'alt' => 'Example Product' ) ) ?>
-            </div>
-            <div class="right_part">
-                <div class="discount_sticker">
-                    <span class="old">$199</span>
-                    <span class="new">$122</span>
-                </div>
-                <h1>Stylish Jacket</h1>
-                <div class="product_description">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ab iure nihil quidem odit architecto temporibus aperiam nobis rerum..
-                </div>
-                <a href="#" class="btn btn-large btn-danger">Add to cart</a>
-            </div>
+            <ul>
+                <li>
+                    <div class="left_part">
+                        <?php echo Asset::img( 'example-product.png', array( 'alt' => 'Example Product' ) ) ?>
+                    </div>
+                    <div class="right_part">
+                        <div class="discount_sticker">
+                            <span class="old">$199</span>
+                            <span class="new">$122</span>
+                        </div>
+                        <h1>Stylish Jacket</h1>
+                        <div class="product_description">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ab iure nihil quidem odit architecto temporibus aperiam nobis rerum..
+                        </div>
+                        <a href="#" class="btn btn-large btn-danger">Add to cart</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="left_part">
+                        <?php echo Asset::img( 'example-product.png', array( 'alt' => 'Example Product' ) ) ?>
+                    </div>
+                    <div class="right_part">
+                        <div class="discount_sticker">
+                            <span class="old">$199</span>
+                            <span class="new">$122</span>
+                        </div>
+                        <h1>Stylish Jacket</h1>
+                        <div class="product_description">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ab iure nihil quidem odit architecto temporibus aperiam nobis rerum..
+                        </div>
+                        <a href="#" class="btn btn-large btn-danger">Add to cart</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="left_part">
+                        <?php echo Asset::img( 'example-product.png', array( 'alt' => 'Example Product' ) ) ?>
+                    </div>
+                    <div class="right_part">
+                        <div class="discount_sticker">
+                            <span class="old">$199</span>
+                            <span class="new">$122</span>
+                        </div>
+                        <h1>Stylish Jacket</h1>
+                        <div class="product_description">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ab iure nihil quidem odit architecto temporibus aperiam nobis rerum..
+                        </div>
+                        <a href="#" class="btn btn-large btn-danger">Add to cart</a>
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
 </div>
